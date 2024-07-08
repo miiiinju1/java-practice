7227ea143b4ed8c781bb41082ba68ddef0d10cb9
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-08 14:38:49 +0900

    feat: step3 @Test 애노테이션 메소드 실행

--- src/test/java/next/reflection/Junit4Runner.java
+++ src/test/java/next/reflection/Junit4Runner.java
@@ -2,10 +2,26 @@ package next.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 public class Junit4Runner {
     @Test
     public void run() throws Exception {
         Class clazz = Junit4Test.class;
+
+        Method[] methods = clazz.getMethods();
+
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                Constructor<?> constructor = clazz.getDeclaredConstructor();
+                Object instance = constructor.newInstance();
+
+                // 메서드 호출
+                method.invoke(instance);
+            }
+
+        }
 
     }
 }
